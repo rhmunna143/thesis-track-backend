56ddb42edb21e2c9732599e33435fa6547ec0070
rhmunna143 <rhmunna19@gmail.com>
2025-05-23 21:27:18 +0600

    Remove backend project files and database migration scripts for thesis track system

--- src/middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(err.status || 500).json({
-        message: err.message || 'Internal Server Error',
-        error: process.env.NODE_ENV === 'development' ? err : {}
-    });
-};
-
-module.exports = errorHandler;
--- src/routes/commentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createComment } = require('../controllers/commentController');
-const { authenticate } = require('../middlewares/authMiddleware');
-
-const router = express.Router();
-
-router.post('/', authenticate, createComment);
-
-module.exports = router;
